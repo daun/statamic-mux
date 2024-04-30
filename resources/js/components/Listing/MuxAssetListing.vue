7eddb8ff4fc90ab77ee4e528b8330b14c328889c
Philipp Daun <post@philippdaun.net>
2024-05-01 00:21:14 +0200

    Fix vendor path

--- resources/js/components/Listing/MuxAssetListing.vue
+++ resources/js/components/Listing/MuxAssetListing.vue
@@ -118,7 +118,7 @@
 </template>
 
 <script>
-import Listing from '../../../../../../../vendor/statamic/cms/resources/js/components/Listing.vue';
+import Listing from '../../../../vendor/statamic/cms/resources/js/components/Listing.vue';
 
 export default {
 
