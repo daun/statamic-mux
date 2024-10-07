62a46d602a071798fe2a4dd061d92fadf73a82b6
Philipp Daun <post@philippdaun.net>
2024-10-07 20:06:11 +0200

    Set up custom layout slots

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import DefaultTheme from 'vitepress/theme'
+  import Image from '../assets/video.png'
+
+  const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-image>
+      <div class="home-hero-image">
+        <img :src="Image" alt="Video" />
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<style scoped>
+  div {
+    margin: 0 16px;
+  }
+  img {
+    width: 100%;
+    max-width: 24em;
+    height: auto;
+    border-radius: 10px;
+  }
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,17 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+
+// import Layout from './Layout.vue'
 import './style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+  Layout: DefaultTheme.Layout
 }
