b286f34c4494f8f74eb1c11a95e5fb1084f3673e
Philipp Daun <post@philippdaun.net>
2024-10-10 17:18:47 +0200

    Test config getters

--- tests/Feature/MuxServiceTest.php
+++ tests/Feature/MuxServiceTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Daun\StatamicMux\Mux\Enums\MuxPlaybackPolicy;
 use Daun\StatamicMux\Mux\MuxApi;
 use Daun\StatamicMux\Mux\MuxService;
 use Daun\StatamicMux\Support\MirrorField;
@@ -36,4 +37,28 @@ test('returns the configuration state', function () {
 
     config(['mux.credentials.token_id' => 'test', 'mux.credentials.token_secret' => 'test']);
     expect($this->service->configured())->toBeTrue();
+});
+
+test('returns the default playback policy', function () {
+    config(['mux.playback_policy' => null]);
+    expect($this->service->getDefaultPlaybackPolicy())->toBeNull();
+
+    config(['mux.playback_policy' => 'public']);
+    expect($this->service->getDefaultPlaybackPolicy())->toBeInstanceOf(MuxPlaybackPolicy::class);
+    expect($this->service->getDefaultPlaybackPolicy()->isPublic())->toBeTrue();
+
+    config(['mux.playback_policy' => 'signed']);
+    expect($this->service->getDefaultPlaybackPolicy())->toBeInstanceOf(MuxPlaybackPolicy::class);
+    expect($this->service->getDefaultPlaybackPolicy()->isSigned())->toBeTrue();
+});
+
+test('returns the default playback modifiers', function () {
+    config(['mux.playback_modifiers' => null]);
+    expect($this->service->getDefaultPlaybackModifiers())->toBeArray()->toHaveCount(0);
+
+    config(['mux.playback_modifiers' => []]);
+    expect($this->service->getDefaultPlaybackModifiers())->toBeArray()->toHaveCount(0);
+
+    config(['mux.playback_modifiers' => ['width' => 100, 'height' => 100]]);
+    expect($this->service->getDefaultPlaybackModifiers())->toEqual(['width' => 100, 'height' => 100]);
 });
