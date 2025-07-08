a03e21a1125fbf62a8a07f6253333105af47567c
Philipp Daun <post@philippdaun.net>
2025-07-08 19:36:14 +0200

    Add job for creating proxy version
    Signed-off-by: Philipp Daun <post@philippdaun.net>

---
+++ src/Jobs/CreateProxyVersionJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Daun\StatamicMux\Jobs;
+
+use Daun\StatamicMux\Mux\Actions\CreateProxyVersion;
+use Daun\StatamicMux\Support\Queue;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Statamic\Assets\Asset;
+
+class CreateProxyVersionJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(
+        protected Asset $asset
+    ) {
+        $this->connection = Queue::connection();
+        $this->queue = Queue::queue();
+    }
+
+    public function handle(CreateProxyVersion $action): void
+    {
+        $action->handle($this->asset);
+    }
+}
