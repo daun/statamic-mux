8b0d2e7093843d55cf95b2084cc818f7c051decc
Philipp Daun <post@philippdaun.net>
2025-10-12 10:14:08 +0200

    Create thumbnail service
    Signed-off-by: Philipp Daun <post@philippdaun.net>

---
+++ src/Thumbnails/ThumbnailService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Daun\StatamicMux\Thumbnails;
+
+use Daun\StatamicMux\Mux\MuxService;
+use Statamic\Assets\Asset;
+
+class ThumbnailService
+{
+    protected int $width = 400;
+
+    public function __construct(
+        protected MuxService $service,
+    ) {}
+
+    public function forAsset(Asset $asset): ?string
+    {
+        if (! $this->service->getMuxId($asset)) {
+            return null;
+        }
+
+        // If playback id already exists, generate gif url immediately
+        // Otherwise, delegate generation to custom route in the background
+        $playbackId = $this->service->getPlaybackId($asset, requestIfMissing: false);
+
+        return $playbackId
+            ? $this->service->getGifUrl($playbackId, ['width' => $this->width])
+            : cp_route('mux.thumbnail', ['container' => $asset->container()->id(), 'path' => $asset->path()]);
+    }
+}
