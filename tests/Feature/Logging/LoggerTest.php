7f2c822dcb5f6c0529d95076f6d5a632e9f5d2d2
Philipp Daun <post@philippdaun.net>
2025-11-19 17:26:44 +0100

    Add scrubber tests (wip)

--- tests/Feature/Logging/LoggerTest.php
+++ tests/Feature/Logging/LoggerTest.php
@@ -39,6 +39,7 @@ it('respects the channel from the config', function () {
     ]);
 
     $logger = makePackageLogger()->resolveChannel();
+    ray($logger);
 
     $logger->debug('mux channel message');
 
@@ -57,4 +58,42 @@ it('respects the level from the config', function () {
     makePackageLogger();
 
     expect(config('logging.channels.mux.level'))->toBe('error');
+});
+
+it('redacts sensitive data from the output', function () {
+    $inMemory = new InMemoryLogger;
+
+    Log::extend('in-memory', fn () => $inMemory);
+
+    config()->set('mux.logging.enabled', true);
+    config()->set('mux.logging.channel', 'mux');
+    config()->set('mux.logging.level', 'debug');
+
+    $factory = makePackageLogger();
+
+    config()->set('logging.channels.mux.driver', 'in-memory');
+
+    $logger = $factory->resolveChannel();
+
+    ray($logger);
+    ray(config('mux.logging'));
+    ray(config('logging'));
+
+    $logger->debug('message with params', [
+        'safe_param' => 'this is safe to log',
+        'token_id' => 'dsg3f87f',
+        'token_secret' => 'sdf9hsd9f8f7',
+        'key_id' => '5hfdligj3s',
+        'private_key' => '809dsfgju54htjdsf0fgujj45ifkrdsifghug8jsdugbshj4i8j',
+    ]);
+
+    $records = $inMemory->records;
+
+    expect($records)->toHaveCount(1);
+    expect($records[0]['message'])->toBe('message with params');
+    expect($records[0]['context']['safe_param'])->toBe('this is safe to log');
+    expect($records[0]['context']['token_id'])->toBe('dsg3****');
+    expect($records[0]['context']['token_secret'])->toBe('sdf9********');
+    expect($records[0]['context']['key_id'])->toBe('5hfd******');
+    expect($records[0]['context']['private_key'])->toBe('809d************************************');
 });
