c265fb49ca9df4f6458a8a4f2a87f5f72edfc2d3
Philipp Daun <post@philippdaun.net>
2025-11-21 19:53:05 +0100

    Add facade documentation
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Facades/Log.php
+++ src/Facades/Log.php
@@ -6,7 +6,17 @@ use Daun\StatamicMux\Support\Logging\LoggerInterface;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Daun\StatamicMux\Support\Logging\Logger
+ * @method static void emergency(\Stringable|string $message, mixed[] $context = [])
+ * @method static void alert(\Stringable|string $message, mixed[] $context = [])
+ * @method static void critical(\Stringable|string $message, mixed[] $context = [])
+ * @method static void error(\Stringable|string $message, mixed[] $context = [])
+ * @method static void warning(\Stringable|string $message, mixed[] $context = [])
+ * @method static void notice(\Stringable|string $message, mixed[] $context = [])
+ * @method static void info(\Stringable|string $message, mixed[] $context = [])
+ * @method static void debug(\Stringable|string $message, mixed[] $context = [])
+ * @method static void log(mixed $level, \Stringable|string $message, mixed[] $context = [])
+ *
+ * @see \Daun\StatamicMux\Support\Logging\LoggerInterface
  */
 class Log extends Facade
 {
