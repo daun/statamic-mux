4de18f5d8c77fed11ab18173b8b5944a448f9805
Philipp Daun <post@philippdaun.net>
2025-11-22 00:26:03 +0100

    Register log channel as stack
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Support/Logging/Logger.php
+++ src/Support/Logging/Logger.php
@@ -13,7 +13,7 @@ class Logger
         protected ?string $channel = null,
         protected bool $enabled = true,
     ) {
-        $this->registerChannel();
+        $this->registerStack();
     }
 
     public function resolveChannel(): PsrLogger
@@ -23,24 +23,42 @@ class Logger
         }
 
         try {
-            return $this->log->channel($this->channel);
+            return $this->log->channel('mux_stack');
         } catch (\Throwable $e) {
             return $this->log; // default app logger
         }
     }
 
-    public function registerChannel(): void
+    public function registerStack(): void
     {
-        if (config('logging.channels.mux')) {
-            return;
+        // 1) Package channel
+        // Can be overwritten by defining 'mux' channel in app config or by setting
+        // the mux.logging.channel to something else.
+        if (! config('logging.channels.mux')) {
+            config()->set('logging.channels.mux', [
+                'driver' => 'daily',
+                'path' => storage_path('logs/mux.log'),
+                'level' => config('mux.logging.level', 'warning'),
+                'days' => config('logging.channels.daily.days', 14),
+                'replace_placeholders' => true,
+                'tap' => [Scrubber::class],
+            ]);
         }
 
-        config()->set('logging.channels.mux', [
-            'driver' => 'daily',
-            'path' => storage_path('logs/mux.log'),
-            'level' => config('mux.logging.level', 'warning'),
-            'days' => config('logging.channels.daily.days', 14),
-            'replace_placeholders' => true,
+        // 2) Passthrough channel to the app’s default, restricted to >= error
+        config()->set('logging.channels.mux_errors', [
+            ...config(
+                sprintf('logging.channels.%s', config('logging.default', 'stack')),
+                ['driver' => 'stack', 'channels' => ['single']]
+            ),
+            'level' => 'error',
+        ]);
+
+        // 3) Final stack exposed by the package
+        config()->set('logging.channels.mux_stack', [
+            'driver' => 'stack',
+            'channels' => [$this->channel, 'mux_errors'],
+            'ignore_exceptions' => false,
             'tap' => [Scrubber::class],
         ]);
     }
