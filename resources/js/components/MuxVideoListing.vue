cc9fe2dc131f993ee6445fa0cff52f21e6228917
Philipp Daun <post@philippdaun.net>
2026-06-07 21:37:02 +0200

    Use builtin clipboard copy callback

--- resources/js/components/MuxVideoListing.vue
+++ resources/js/components/MuxVideoListing.vue
@@ -331,29 +331,8 @@ export default {
         },
 
         async copyToClipboard(value) {
-            if (!value) {
-                Statamic.$toast.error(__('Nothing to copy'));
-                return;
-            }
-
-            try {
-                if (navigator.clipboard?.writeText) {
-                    await navigator.clipboard.writeText(value);
-                } else {
-                    const textarea = document.createElement('textarea');
-                    textarea.value = value;
-                    textarea.setAttribute('readonly', '');
-                    textarea.style.position = 'absolute';
-                    textarea.style.left = '-9999px';
-                    document.body.appendChild(textarea);
-                    textarea.select();
-                    document.execCommand('copy');
-                    document.body.removeChild(textarea);
-                }
-                Statamic.$toast.success(__('Copied to clipboard'));
-            } catch (e) {
-                console.error(e);
-                Statamic.$toast.error(__('Failed to copy to clipboard'));
+            if (value) {
+                Statamic.$callbacks.call('copyToClipboard', value);
             }
         },
 
