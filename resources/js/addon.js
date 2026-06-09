9618d12fc2632988bc39fa8388907e5e42c18f98
Philipp Daun <post@philippdaun.net>
2026-06-09 23:53:40 +0200

    Add custom icon set

--- resources/js/addon.js
+++ resources/js/addon.js
@@ -1,3 +1,5 @@
+import { registerIconSet } from '@statamic/cms/ui';
+
 import MuxMirrorFieldtype from './components/MuxMirrorFieldtype.vue'
 import MuxMirrorIndexFieldtype from './components/MuxMirrorIndexFieldtype.vue'
 import MirroredAssetsPage from './pages/MirroredAssets.vue'
@@ -7,3 +9,5 @@ Statamic.$components.register('mux_mirror-fieldtype', MuxMirrorFieldtype)
 Statamic.$components.register('mux_mirror-fieldtype-index', MuxMirrorIndexFieldtype)
 Statamic.$inertia.register('MuxAssetsPage', MirroredAssetsPage)
 Statamic.$inertia.register('MuxLibraryPage', MuxLibraryPage)
+
+registerIconSet('mux', import.meta.glob('../svg/icons/*.svg', { query: '?raw', import: 'default' }));
