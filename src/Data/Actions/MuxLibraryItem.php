671c567d4676d2fd8d01900982ea16690e6cdb0f
Philipp Daun <post@philippdaun.net>
2026-06-12 19:11:46 +0200

    Slim down library item

--- src/Data/Actions/MuxLibraryItem.php
+++ src/Data/Actions/MuxLibraryItem.php
@@ -2,7 +2,7 @@
 
 namespace Daun\StatamicMux\Data\Actions;
 
-use MuxPhp\Models\Asset as MuxApiAsset;
+use MuxPhp\Models\Asset as MuxApiAssetModel;
 
 /**
  * Lightweight wrapper around a remote Mux asset for use with the Statamic Action system.
@@ -11,7 +11,7 @@ class MuxLibraryItem
 {
     public function __construct(
         protected string $muxId,
-        protected ?MuxApiAsset $apiAsset = null,
+        protected ?MuxApiAssetModel $apiAsset = null,
         protected ?string $dashboardBaseUrl = null,
     ) {}
 
@@ -20,53 +20,9 @@ class MuxLibraryItem
         return $this->muxId;
     }
 
-    public function apiAsset(): ?MuxApiAsset
+    public function apiAsset(): ?MuxApiAssetModel
     {
         return $this->apiAsset;
-    }
-
-    public function dashboardUrl(): ?string
-    {
-        if (! $this->dashboardBaseUrl) {
-            return null;
-        }
-
-        return rtrim($this->dashboardBaseUrl, '/').'/video/assets/'.rawurlencode($this->muxId).'/monitor';
-    }
-
-    public function primaryPlaybackId(): ?string
-    {
-        if (! $this->apiAsset) {
-            return null;
-        }
-
-        $playbackIds = collect($this->apiAsset->getPlaybackIds() ?? []);
-
-        $public = $playbackIds->first(function ($pid) {
-            $policy = $pid->getPolicy();
-
-            return $this->normalizePolicy($policy) === 'public';
-        });
-
-        return $public?->getId() ?? $playbackIds->first()?->getId();
-    }
-
-    public function playerUrl(): ?string
-    {
-        $id = $this->primaryPlaybackId();
-
-        return $id ? "https://player.mux.com/{$id}" : null;
-    }
-
-    public function embedCode(): ?string
-    {
-        $url = $this->playerUrl();
-
-        if (! $url) {
-            return null;
-        }
-
-        return '<iframe src="'.$url.'" style="width: 100%; border: none; aspect-ratio: 16/9;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen ></iframe>';
     }
 
     protected function normalizePolicy(mixed $policy): ?string
